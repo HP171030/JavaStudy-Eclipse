c26bfd64ec6bc5d873bb8ac0e568b290c2dd0472
이형필 <hexagongame@naver.com>
2024-11-26 14:45:14 +0900

    feat : Java Commit Test

---
+++ StudyJava/src/java/Car.java
@@ -0,0 +1,10 @@
+package java;
+
+public class Car extends HelloWorld {
+
+	public void Test() {
+		 Car car = new Car();
+		 car.Run();
+	}
+
+}
---
+++ StudyJava/src/java/HelloWorld.java
@@ -0,0 +1,18 @@
+package java;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HelloWorld hello = new HelloWorld();
+		
+		hello.Run();
+		
+	}
+	public void Run() {
+		
+	}
+	
+	
+}
+
